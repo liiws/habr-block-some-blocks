bd74bd9bce38409d5cc58c74b184ae64b25e33ba
liiws <none@none>
2019-03-27 20:55:22 +0300

    [+] news url

--- habr-block-some-blocks.user.js
+++ habr-block-some-blocks.user.js
@@ -4,12 +4,14 @@
 // @include     https://habr.com/ru/post/*
 // @include     https://habr.com/ru/company/*
 // @include     https://habr.com/ru/article/*
+// @include     https://habr.com/ru/news/*
 // @include     https://habr.com/en/post/*
 // @include     https://habr.com/en/company/*
 // @include     https://habr.com/en/article/*
+// @include     https://habr.com/en/news/*
 // @grant       none
 // @run-at      document-start
-// @version     0.5.2
+// @version     0.5.3
 // @downloadURL https://bitbucket.org/liiws/habr-best-comments/downloads/habr-block-some-blocks.user.js
 // @updateURL   https://bitbucket.org/liiws/habr-best-comments/downloads/habr-block-some-blocks.meta.js
 // ==/UserScript==
